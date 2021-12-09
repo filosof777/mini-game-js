fcba862d7933a02110bd65d2a8e3bc2c92f88551
filosof777 <olimjonkamalov@mail.ru>
2021-12-09 14:06:04 +0500

    initial commit

---
+++ js/game.js
@@ -0,0 +1,11 @@
+let games = [
+  {
+    img: `<img src="img/paper.svg" alt="paper">`
+  },
+  {
+    img: `<img src="img/scissors.svg" alt="scissors">`
+  },
+  {
+    img: `<img src="img/stone.svg" alt="stone">`
+  }
+];
--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-let elNavList = document.querySelector('.nav__list');
-let elHeaderBtn = document.querySelector('.header__btn');
+let elModalGame = document.querySelector('.modal-game');
+let elHands = document.querySelector('.content');
+let elModalBtn = document.querySelector('.modal-game__btn');
 
-elHeaderBtn.addEventListener('click', () => {
-  elNavList.classList.toggle('show-menu');
-});
+let touchingFunc = (hand) => {
+  elHands.style.display = 'none';
+  elModalGame.style.display = 'flex';
+  document.querySelector('.modal-game__img').src = `/img/${hand}.svg`;
+
+  gameFunc()
+}
+
+
+elModalBtn.addEventListener('click', modalClose);
+
+function modalClose() {
+  elModalGame.style.display = 'none';
+  elHands.style.display = 'flex';
+}
+
+let gameFunc = () => {
+  let arr = ["paper", "scissors", "stone"];
+  let comp = arr[Math.floor(Math.random() * 3)];
+  document.querySelector('#comp').src = `/img/${comp}.svg`;
+  return comp
+}
+
+
 
